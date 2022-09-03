fa756cadd8cfbd70b6012c84ddd9e4b164f08286
Niels Verleysen <nlsverleysen99@gmail.com>
2022-09-03 13:05:02 +0200

    Hide carousel items and fix projects image

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,10 @@
 let slideIndex = [1,1];
 /* Class the members of each slideshow group with different CSS classes */
 let slideId = ["slides1", "slides2"]
+let x = document.getElementsByClassName("slides1", "slides2");
+for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
 showSlides(1, 0);
 showSlides(1, 1);
 
